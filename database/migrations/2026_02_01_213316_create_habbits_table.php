069a516517c34a9b6ebefefd4cc65f13fa00bd20
Miguel0180 <miguelmacedo2209@gmail.com>
2026-02-01 18:47:06 -0300

    Setup the database for users

--- database/migrations/2026_02_01_213316_create_habbits_table.php
+++ database/migrations/2026_02_01_213316_create_habbits_table.php
@@ -11,17 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('habbits', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
             $table->timestamps();
+            
         });
-
-
-
-
     }
 
     /**
@@ -29,7 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
-
+        Schema::dropIfExists('habbits');
     }
 };
